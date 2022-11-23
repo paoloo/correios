05e409f0ee305bbd740476c1cebf8920337e1644
Paolo Oliveira <paolocmo@gmail.com>
2022-11-23 01:02:10 -0300

    update data retrieval method

--- pacote.go
+++ pacote.go
@@ -6,25 +6,49 @@ import (
   "net/http"
   "io/ioutil"
   "encoding/json"
+  b64 "encoding/base64"
 )
 
+type tokenType struct {
+  Token string `json:"token"`
+}
+
 func BuscaEncomenda(_code string) (string, string){
-  apiUrl := "http://webservice.correios.com.br/service/rest/rastro/rastroMobile"
-  data := "<rastroObjeto><usuario>MobileXect</usuario><senha>DRW0#9F$@0</senha><tipo>L</tipo><resultado>U</resultado><objetos>"+ _code  +"</objetos><lingua>101</lingua><token>QTXFMvu_Z-6XYezP3VbDsKBgSeljSqIysM9x</token></rastroObjeto>"
+  // =============================================== phase#0 data
+  validationUrl := "https://proxyapp.correios.com.br/v1/app-validation"
+  data := []byte(`{"requestToken":"`+ b64.StdEncoding.EncodeToString([]byte("android;br.com.correios.preatendimento;F32E29976709359859E0B97F6F8A483B9B953578")) +`"}`)
+  apiURL := "https://proxyapp.correios.com.br/v1/sro-rastro/"+_code
+  // =============================================== phase#1 validation
   client := &http.Client{}
-  r, err := http.NewRequest("POST", apiUrl, bytes.NewBufferString(data)); if err != nil {
+  r, err := http.NewRequest("POST", validationUrl, bytes.NewBuffer(data)); if err != nil {
     return "-1 ERROR","cannot craft request!"
   }
   r.Header.Add("Accept", "application/json")
-  r.Header.Add("Content-Type", "application/xml")
-  r.Header.Add("User-Agent","Dalvik/1.6.0 (Linux; U; Android 4.2.1; LG-P875h Build/JZO34L)")
+  r.Header.Add("Content-Type", "application/json")
+  r.Header.Add("User-Agent","Dart/2.18 (dart:io)")
   resp, err := client.Do(r); if err != nil {
     return "-2 ERROR","cannot send request!"
   }
   _body, err := ioutil.ReadAll(resp.Body); if err != nil {
     return "-3 ERROR", "cannot decode body!"
   }
-  return resp.Status, string(_body)
+  res := tokenType{}
+  json.Unmarshal(_body, &res)
+  // =============================================== phase#2 search
+  client2 := &http.Client{}
+  r2, err := http.NewRequest("GET", apiURL, nil); if err != nil {
+    return "-4 ERROR","cannot craft request!"
+  }
+  r2.Header.Add("User-Agent","Dart/2.18 (dart:io)")
+  r2.Header.Add("Accept", "application/json")
+  r2.Header.Add("app-check-token", res.Token)
+  resp2, err := client2.Do(r2); if err != nil {
+    return "-5 ERROR", "cannot send request"
+  }
+  _body2, err := ioutil.ReadAll(resp2.Body); if err != nil {
+    return "-6 ERROR", "cannot decode body!"
+  }
+  return resp2.Status, string(_body2)
 }
 
 func PrintEncomenda(_jsonblob string){
@@ -32,4 +56,3 @@ func PrintEncomenda(_jsonblob string){
   json.Indent(buf, []byte(_jsonblob), "", "  ")
   fmt.Println(buf)
 }
-
